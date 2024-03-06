a032867412d3c0e79e931e45c55a8796999c6140
Paul Boniol <paul.boniol@free.fr>
2024-03-06 17:04:07 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -31,7 +31,7 @@ def run():
     
     graph,X,y = read_dataset(dataset)
     fig_graph = create_graph(graph)
-    st.plotly_chart(fig_graph, use_container_width=True,height=800)
+    #st.plotly_chart(fig_graph, use_container_width=True,height=800)
     selected_node = plotly_events(fig_graph)
 
     st.markdown("Selected node is {}".format(selected_node))
