0a2931a5bdaae2313385d41ec3f28678594d2202
Paul Boniol <paul.boniol@free.fr>
2024-03-06 16:16:47 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -30,6 +30,7 @@ def run():
     
     graph,X,y = read_dataset(dataset)
     fig_graph = create_graph(graph)
+    st.plotly_chart(fig_graph, use_container_width=True)
 
 
 if __name__ == "__main__":
