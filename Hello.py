c1cac43598266630d5de01ea6b6801d2d76effe4
Paul Boniol <paul.boniol@free.fr>
2024-03-07 09:09:06 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -48,7 +48,8 @@ def run():
     tab_ts,tab_graph,tab_detail = st.tabs(["Time series", "Graph", "Under the hood"])
 
     with tab_ts:
-        st.markdown("show ts")
+        fig_ts = show_ts(X,y)
+        st.plotly_chart(fig_ts, use_container_width=True,height=800)
     
     with tab_graph:
         fig_graph,node_label = create_graph(graph)
