33988cb681b96d6192e011dfbaa48e8d053e98e2
Paul Boniol <paul.boniol@free.fr>
2024-03-08 10:47:05 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -63,11 +63,11 @@ def run():
             the graph on the graph tab for more details. Only 50 first time series are displayed.""")
             st.plotly_chart(fig_pred, use_container_width=True,height=800)
 
-        with st.expander("Time series dataset (clustered by $k$-Shape, ARI: {})".format(adjusted_rand_score(y,y_pred_kshape)))):
+        with st.expander("Time series dataset (clustered by $k$-Shape, ARI: {})".format(adjusted_rand_score(y,y_pred_kshape))):
             st.markdown("""Time series grouped based on the clustering labels of $k$-Shape. Only 50 first time series are displayed.""")
             st.plotly_chart(fig_pred_kshape, use_container_width=True,height=800)
 
-        with st.expander("Time series dataset (clustered by $k$-Means, ARI: {})".format(adjusted_rand_score(y,y_pred_kmeans)):
+        with st.expander("Time series dataset (clustered by $k$-Means, ARI: {})".format(adjusted_rand_score(y,y_pred_kmeans))):
             st.markdown("""Time series grouped based on the clustering labels of $k$-Means. Only 50 first time series are displayed.""")
             st.plotly_chart(fig_pred_kmean, use_container_width=True,height=800)
         
