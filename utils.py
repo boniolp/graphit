d15d17dd7161ca1d89cd1c24456034f9b31e93a4
Paul Boniol <paul.boniol@free.fr>
2024-10-30 11:21:38 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -200,7 +200,7 @@ def show_ts_node(X,y,y_pred_kgraph,intervals):
         lab = y[interval[0]]
         pred = y_pred_kgraph[interval[0]]
         fig_pred.add_trace(
-            go.Scattergl(x=x_list[interval[1]:interval[2]], y=x[interval[1]:interval[2]], mode='lines', line_color="grey",
+            go.Scattergl(x=x_list[interval[1]:interval[2]], y=x[interval[1]:interval[2]], mode='lines', line_color="grey"),
             row=1, col=labels_pred[pred]+1
         )
     fig_pred.update_layout(height=400,title="ARI: {}".format(adjusted_rand_score(y_pred_kgraph,y)))
