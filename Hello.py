1efb9a77f9dfdc8d4e6f242f2732e20828305bbb
Paul Boniol <paul.boniol@free.fr>
2024-10-30 11:50:31 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -57,32 +57,36 @@ def run():
 
     with tab_ts:
         st.header("Compare $k$-Graph with $k$-Shape, and the usual baseline $k$-Means")
-        with st.expander("",expanded=True):
-            fig_ts,fig_pred,fig_pred_kshape,fig_pred_kmean = show_ts(X,y,graph['kgraph_labels'],y_pred_kshape,y_pred_kmean)
-            st.markdown("""You can compare below the clustering performances (using the [Adjusted Rand Index]()) of our porposed approach 
-            $k$-Graph, with the state-of-the-art clustering algorithm $k$-Shape, and the usual baseline $k$-Means.""")
-            
-            with st.expander("$k$-Graph, ARI: {:.3f}".format(adjusted_rand_score(y,graph['kgraph_labels'])),expanded=True):
-                st.markdown("""Time series grouped based on the clustering labels of $k$-Graph. You can check 
-                the graph on the graph tab for more details. Only 50 first time series are displayed.""")
-                st.plotly_chart(fig_pred, use_container_width=True,height=800)
-    
-            with st.expander("$k$-Shape, ARI: {:.3f}".format(adjusted_rand_score(y,y_pred_kshape)),expanded=True):
-                st.markdown("""Time series grouped based on the clustering labels of $k$-Shape. Only 50 first time series are displayed.""")
-                st.plotly_chart(fig_pred_kshape, use_container_width=True,height=800)
-    
-            with st.expander("$k$-Means, ARI: {:.3f}".format(adjusted_rand_score(y,y_pred_kmean)),expanded=True):
-                st.markdown("""Time series grouped based on the clustering labels of $k$-Means. Only 50 first time series are displayed.""")
-                st.plotly_chart(fig_pred_kmean, use_container_width=True,height=800)
-    
-            st.markdown("""You can find below the time series grouped using the true labels.""")
-            
-            with st.expander("Time series dataset (true labels)"):
-                st.markdown("""Time series grouped based on the true labels 
-                (see [UCR-Archive](https://www.cs.ucr.edu/%7Eeamonn/time_series_data_2018/) for more details. 
-                Only 50 first time series are displayed.)""")
-            
-                st.plotly_chart(fig_ts, use_container_width=True,height=800)
+        st.divider()
+        fig_ts,fig_pred,fig_pred_kshape,fig_pred_kmean = show_ts(X,y,graph['kgraph_labels'],y_pred_kshape,y_pred_kmean)
+        st.markdown("""You can compare below the clustering performances (using the [Adjusted Rand Index]()) of our porposed approach 
+        $k$-Graph, with the state-of-the-art clustering algorithm $k$-Shape, and the usual baseline $k$-Means.""")
+        
+        with st.expander("$k$-Graph, ARI: {:.3f}".format(adjusted_rand_score(y,graph['kgraph_labels'])),expanded=True):
+            st.markdown("""Time series grouped based on the clustering labels of $k$-Graph. You can check 
+            the graph on the graph tab for more details. Only 50 first time series are displayed.""")
+            st.plotly_chart(fig_pred, use_container_width=True,height=800)
+
+        with st.expander("$k$-Shape, ARI: {:.3f}".format(adjusted_rand_score(y,y_pred_kshape)),expanded=True):
+            st.markdown("""Time series grouped based on the clustering labels of $k$-Shape. Only 50 first time series are displayed.""")
+            st.plotly_chart(fig_pred_kshape, use_container_width=True,height=800)
+
+        with st.expander("$k$-Means, ARI: {:.3f}".format(adjusted_rand_score(y,y_pred_kmean)),expanded=True):
+            st.markdown("""Time series grouped based on the clustering labels of $k$-Means. Only 50 first time series are displayed.""")
+            st.plotly_chart(fig_pred_kmean, use_container_width=True,height=800)
+
+        st.markdown("""You can find below the time series grouped using the true labels.""")
+        
+        with st.expander("Time series dataset (true labels)"):
+            st.markdown("""Time series grouped based on the true labels 
+            (see [UCR-Archive](https://www.cs.ucr.edu/%7Eeamonn/time_series_data_2018/) for more details. 
+            Only 50 first time series are displayed.)""")
+        
+            st.plotly_chart(fig_ts, use_container_width=True,height=800)
+        
+        st.header("Overall benchmark with 14 baselines")
+        st.divider()
+        st.markdown("""We compare the """)
     
     with tab_graph:
 
